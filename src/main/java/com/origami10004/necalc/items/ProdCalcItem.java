ef5c73b3caa5120967320c4150e64f3a0eda2d4b
Traister101 <66151907+Traister101@users.noreply.github.com>
2026-06-06 22:38:20 -0700

    Do item registration better

---
+++ src/main/java/com/origami10004/necalc/items/ProdCalcItem.java
@@ -0,0 +1,28 @@
+package com.origami10004.necalc.items;
+
+import com.origami10004.necalc.gui.guiProductionCalc;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.*;
+import net.minecraft.util.*;
+import net.minecraft.world.World;
+
+public class ProdCalcItem extends Item {
+
+	public ProdCalcItem() {
+		setMaxStackSize(1);
+		setHasSubtypes(true);
+		setMaxDamage(0);
+	}
+
+	@Override
+	public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand handIn) {
+		if (worldIn.isRemote) {
+			// open GUI
+			Minecraft.getMinecraft().displayGuiScreen(new guiProductionCalc(playerIn.inventory));
+		}
+
+		return new ActionResult<>(EnumActionResult.SUCCESS, playerIn.getHeldItem(handIn));
+	}
+}
